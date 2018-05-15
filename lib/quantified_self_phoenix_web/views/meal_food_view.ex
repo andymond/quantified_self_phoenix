f7a94df770b59c38e29fc0eaf7d07bc7658d45ee
Andrew Dymond <andy.dymond@gmail.com>
2018-05-15 14:51:57 -0600

    Add deleted.json to meal_food_view

--- lib/quantified_self_phoenix_web/views/meal_food_view.ex
+++ lib/quantified_self_phoenix_web/views/meal_food_view.ex
@@ -6,8 +6,12 @@ defmodule QuantifiedSelfPhoenixWeb.MealFoodView do
     %{data: render_many(mealfoods, MealFoodView, "meal_food.json")}
   end
 
-  def render("success.json", %{names: names}) do
+  def render("created.json", %{names: names}) do
     %{message: "Successfully added #{names.food} to #{names.meal}"}
+  end
+
+  def render("deleted.json", %{names: names}) do
+    %{message: "Successfully removed #{names.food} from #{names.meal}"}
   end
 
   def render("meal_food.json", %{meal_food: meal_food}) do
