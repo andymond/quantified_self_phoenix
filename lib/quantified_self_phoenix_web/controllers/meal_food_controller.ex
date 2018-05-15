0f9c5542f6232eaf77e2a6572010be5722556e1a
Andrew Dymond <andy.dymond@gmail.com>
2018-05-15 14:52:20 -0600

    Update delete action to render proper deletion message

--- lib/quantified_self_phoenix_web/controllers/meal_food_controller.ex
+++ lib/quantified_self_phoenix_web/controllers/meal_food_controller.ex
@@ -14,14 +14,18 @@ defmodule QuantifiedSelfPhoenixWeb.MealFoodController do
     with {:ok, %MealFood{}} <- MealFoods.create_meal_food(meal_food_params) do
       conn
       |> put_status(:created)
-      |> render("success.json", names: %{meal: meal.name, food: food.name})
+      |> render("created.json", names: %{meal: meal.name, food: food.name})
     end
   end
-  #
-  # def delete(conn, %{"id" => id}) do
-  #   meal_food = MealFoods.get_meal_food!(id)
-  #   with {:ok, %MealFood{}} <- MealFoods.delete_meal_food(meal_food) do
-  #     send_resp(conn, :no_content, "")
-  #   end
-  # end
+
+  def delete(conn, %{"food_id" => food_id, "meal_id" => meal_id} = meal_food_params) do
+    meal = Meals.get_meal!(meal_id)
+    food = Foods.get_food!(food_id)
+    meal_food = MealFoods.find_meal_food(for {key, val} <- meal_food_params, into: %{}, do: {String.to_atom(key), val})
+    with {:ok, %MealFood{}} <- MealFoods.delete_meal_food(meal_food) do
+      conn
+      |> put_status(:ok)
+      |> render("deleted.json", names: %{meal: meal.name, food: food.name})
+    end
+  end
 end
